707b6155996279dc6ef74ee4f1bcd570bf0ed932
s7799653 <s7799653@qq.com>
2017-10-16 15:20:17 +0800

    Modify
    Modify tinyshop1 because homeword is changed
    Modify click_and_input :delete sleep

--- selenium/tinyshop/tinyshop.py
+++ selenium/tinyshop/tinyshop.py
@@ -5,11 +5,16 @@ from selenium import webdriver
 wd=webdriver.Chrome()
 url="http://172.18.24.42/tinyshop/"
 open_URL(wd,url)
-e_click_by_link(wd,'登录')
-input_by_id(wd,'account','s123@123.123')
-input_by_xpath(wd,'/html/body/div[2]/div/div/form/ul/li[2]/input','123456')
-e_click_by_xpath(wd,'/html/body/div[2]/div/div/form/ul/li[4]/button')
-if is_element_exist(wd,"安全退出"):
+def login(username,passwd):
+    e_click_by_link(wd,'登录')
+    input_by_id(wd,'account',username)
+    input_by_xpath(wd,'/html/body/div[2]/div/div/form/ul/li[2]/input',passwd)
+    e_click_by_xpath(wd,'/html/body/div[2]/div/div/form/ul/li[4]/button')
+    if is_element_exist(wd,"安全退出"):
+        return True
+    else:
+        return False
+if login('s123@123.123','123456'):
     print("login successful")
     wd.get_screenshot_as_file("successful.png")
 else:
--- selenium/tinyshop/tinyshop1.py
+++ selenium/tinyshop/tinyshop1.py
@@ -1,12 +1,22 @@
 #coding=utf-8
-import click_and_input
+from click_and_input import *
 from selenium import webdriver
 
-wd=webdriver.Chrome()
+wd=webdriver.Firefox()
 url="http://172.18.24.42/tinyshop/"
 open_URL(wd,url)
 e_click_by_link(wd,'登录')
-input_by_id(wd,'account','s123@123.123')
-input_by_xpath(wd,'/html/body/div[2]/div/div/form/ul/li[2]/input','123456')
-e_click_by_xpath(wd,'/html/body/div[2]/div/div/form/ul/li[4]/button')
+
+username_passwd=[['123@123.123','123456',False],['132','123',False],['s123@123.123','123456',True]]
+for item in username_passwd:
+    input_by_id(wd,'account',item[0])
+    input_by_xpath(wd,'/html/body/div[2]/div/div/form/ul/li[2]/input',item[1])
+    e_click_by_xpath(wd,'/html/body/div[2]/div/div/form/ul/li[4]/button')
+    if is_element_exist(wd,'安全退出') == item[2]:
+        print ("pass")
+    else:
+        print ("failed")
+    if is_element_exist(wd,"安全退出"):
+        break
+e_click_by_link(wd,'安全退出')
 Brower_quit(wd)
