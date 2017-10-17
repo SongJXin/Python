dc16fc74bfb5534bf909f196a54788be0641bde4
s7799653 <s7799653@qq.com>
2017-10-17 11:39:31 +0800

    exception
    add throw timeoutexceptiong

--- selenium/百度/click_and_input.py
+++ selenium/百度/click_and_input.py
@@ -7,7 +7,6 @@ from selenium.webdriver.common.keys import Keys
 import selenium
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
 #open web page
 def open_URL(wd,url):
     wd.get(url)
@@ -20,6 +19,8 @@ def e_click_by_link(wd,str1):
         try:
             WebDriverWait(wd, 10, 0.5).until(EC.presence_of_element_located((By.LINK_TEXT,str1))).click()
             break
+        except selenium.common.exceptions.TimeoutException :
+            print("not search element link = ",str1)
         except Exception as err:
             ActionChains(wd).send_keys(Keys.PAGE_DOWN).perform()
             
@@ -29,10 +30,13 @@ def e_click_by_partial_link(wd,str1):
     #element=WebDriverWait(wd, 20, 0.5).until(EC.presence_of_element_located((By.PARTIAL_LINK_TEXT, str1)))
     while True:
         try:
-            WebDriverWait(wd, 20, 0.5).until(EC.presence_of_element_located((By.PARTIAL_LINK_TEXT, str1))).click()
+            WebDriverWait(wd, 10, 0.5).until(EC.presence_of_element_located((By.PARTIAL_LINK_TEXT, str1))).click()
             break
+        except selenium.common.exceptions.TimeoutException :
+            print("not search element partial_link =%s "%(str1))
         except Exception as err:
             ActionChains(wd).send_keys(Keys.PAGE_DOWN).perform()
+            print(err)
 
             
 #find element and click by id
@@ -42,6 +46,8 @@ def e_click_by_id(wd,str1):
         try:
             WebDriverWait(wd, 10, 0.5).until(EC.presence_of_element_located((By.ID,str1))).click()
             break
+        except selenium.common.exceptions.TimeoutException :
+            print("not search element id = %s"%(str1))
         except Exception as err:
             ActionChains(wd).send_keys(Keys.PAGE_DOWN).perform()
 
@@ -53,6 +59,8 @@ def e_click_by_xpath(wd,str1):
         try:
             WebDriverWait(wd, 10, 0.5).until(EC.presence_of_element_located((By.XPATH,str1))).click()
             break
+        except selenium.common.exceptions.TimeoutException :
+            print("not search element xpath = ",str1)
         except Exception as err:
             ActionChains(wd).send_keys(Keys.PAGE_DOWN).perform()
 
@@ -64,6 +72,8 @@ def e_click_by_css(wd,str1):
         try:
             WebDriverWait(wd, 10, 0.5).until(EC.presence_of_element_located((By.CSS_SELECTOR,str1))).click()
             break
+        except selenium.common.exceptions.TimeoutException :
+            print("not search element css = ",str1)
         except Exception as err:
             ActionChains(wd).send_keys(Keys.PAGE_DOWN).perform()
 
@@ -81,6 +91,8 @@ def input_by_id(wd,id1,str1):
             WebDriverWait(wd, 10, 0.5).until(EC.presence_of_element_located((By.ID,id1))).clear()
             WebDriverWait(wd, 10, 0.5).until(EC.presence_of_element_located((By.ID,id1))).send_keys(str1)
             break
+        except selenium.common.exceptions.TimeoutException :
+            print("not search element id = ",id1)
         except Exception as err:
             ActionChains(wd).send_keys(Keys.PAGE_DOWN).perform()
 
@@ -93,6 +105,8 @@ def input_by_css(wd,css,str1):
             WebDriverWait(wd, 10, 0.5).until(EC.presence_of_element_located((By.CSS_SELECTOR,css))).clear()
             WebDriverWait(wd, 10, 0.5).until(EC.presence_of_element_located((By.CSS_SELECTOR,css))).send_keys(str1)
             break
+        except selenium.common.exceptions.TimeoutException :
+            print("not search element css = ",css)
         except Exception as err:
             ActionChains(wd).send_keys(Keys.PAGE_DOWN).perform() 
 
@@ -105,6 +119,8 @@ def input_by_xpath(wd,xpath,str1):
             WebDriverWait(wd, 10, 0.5).until(EC.presence_of_element_located((By.XPATH,xpath))).clear()
             WebDriverWait(wd, 10, 0.5).until(EC.presence_of_element_located((By.XPATH,xpath))).send_keys(str1)
             break
+        except selenium.common.exceptions.TimeoutException :
+            print("not search element xpat = ",xpath)
         except Exception as err:
             ActionChains(wd).send_keys(Keys.PAGE_DOWN).perform()   
 
